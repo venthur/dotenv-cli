38b723b0600ebd1acb13e948cca5b21222cab206
Bastian Venthur <venthur@debian.org>
2018-10-14 18:27:10 +0200

    refactored stuff into core

--- dotenv_cli/__main__.py
+++ dotenv_cli/__main__.py
@@ -1,8 +1,9 @@
-import sys
-import os
 import argparse
-from subprocess import Popen, PIPE, STDOUT
 import logging
+import sys
+
+from dotenv_cli.core import run_dotenv
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -20,65 +21,15 @@ parser.add_argument(
     nargs=argparse.REMAINDER,
 )
 
-def read_dotenv(dotenv):
-    with open(dotenv, 'r') as fh:
-        data = fh.read()
-
-    res = {}
-    for line in data.splitlines():
-        logger.debug(line)
-
-        line = line.strip()
-
-        # ignore comments
-        if line.startswith('#'):
-            continue
-
-        # ignore empty lines or lines w/o '='
-        if '=' not in line:
-            continue
-
-        key, value = line.split('=', 1)
-
-        key = key.strip()
-        value = value.strip()
-
-        # remove quotes (not sure if this is standard behaviour)
-        if value[0] == value[-1] in ['"', "'"]:
-            value = value[1:-1]
-
-        res[key] = value
-    logger.debug(res)
-    return res
-
 
 def main():
     args = parser.parse_args()
     if not args.command:
         return
 
-    # read dotenv
-    dotenv = read_dotenv(args.dotenv)
-
-    # update env
-    env = os.environ.copy()
-    env.update(dotenv)
-
-
-    # execute
-    proc = Popen(
-        args.command,
-        #stdin=PIPE,
-        #stdout=PIPE,
-        #stderr=STDOUT,
-        universal_newlines=True,
-        bufsize=0,
-        shell=False,
-        env=env,
-    )
-    _, _ = proc.communicate()
-    return proc.returncode
+    return run_dotenv(args.dotenv, args.command)
 
 
 if __name__ == '__main__':
+    # sys exit?
     main()
