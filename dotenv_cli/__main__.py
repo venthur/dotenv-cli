030cfae743ae52fc579f45464fd62e86aa3f9517
Bastian Venthur <venthur@debian.org>
2018-10-14 19:26:06 +0200

    added test for returncode and alternative tempfile

--- dotenv_cli/__main__.py
+++ dotenv_cli/__main__.py
@@ -36,5 +36,4 @@ def main():
 
 
 if __name__ == '__main__':
-    # sys exit?
     main()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 from subprocess import run, PIPE
+import tempfile
 
 
 def test_stdout():
@@ -11,3 +12,23 @@ def test_stderr():
                 'python', '-c', 'import os; os.write(2, b"test")'],
                stderr=PIPE)
     assert b'test' in proc.stderr
+
+
+def test_returncode():
+    proc = run(['dotenv', 'false'])
+    assert proc.returncode == 1
+
+    proc = run(['dotenv', 'true'])
+    assert proc.returncode == 0
+
+
+def test_alternative_dotenv():
+    f = tempfile.NamedTemporaryFile('w')
+    with open(f.name, 'w') as fh:
+        fh.write('foo=bar')
+
+    proc = run(['dotenv', '-e', f.name, 'env'], stdout=PIPE)
+    assert b'foo=bar' in proc.stdout
+
+    proc = run(['dotenv', '--dotenv', f.name, 'env'], stdout=PIPE)
+    assert b'foo=bar' in proc.stdout
